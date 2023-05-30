1855e9aa96a45267b925e756ede9bee048408160
Lucas Gomes <lucasggomees@gmail.com>
2023-05-30 01:49:00 +0100

    Add a new note to the page

--- part2/notesExample/src/App.jsx
+++ part2/notesExample/src/App.jsx
@@ -2,19 +2,62 @@ import React from 'react'
 import Note from './components/Note'
 import { useState } from 'react'
 
-const App = ({ notes }) => {
+const notes = [
+	{
+		id: 1,
+		content: 'HTML is easy',
+		important: true,
+	},
+	{
+		id: 2,
+		content: 'Browser can execute only JavaScript',
+		important: false,
+	},
+	{
+		id: 3,
+		content: 'GET and POST are the most important methods of HTTP protocol',
+		important: true,
+	},
+]
+
+const App = () => {
+	const [notesList, setNotesList] = useState(notes)
+	const [enteredNote, setEnteredNote] = useState('')
+	const [showAll, setShowAll] = useState(true)
+
+	const enteredNoteHandler = (e) => {
+		setEnteredNote(e.target.value)
+	}
+
+	// console.log(enteredNote)
+
+	const formSubmitHandler = (e) => {
+		e.preventDefault()
+		const newNoteData = {
+			id: Math.random(),
+			content: enteredNote,
+			important: Math.random() < 0.5,
+		}
+		setNotesList(notesList.concat(newNoteData)) // setNotesList([...notesList, newNoteData]))
+		// console.log(newNoteData)
+		// console.log(notesList)
+		setEnteredNote('')
+	}
+
 	return (
 		<div>
 			<h1>Notes</h1>
 			<ul>
-				{notes.map((note) => (
+				{notesList.map((note) => (
 					<Note key={note.id} note={note} />
 				))}
 			</ul>
+			<form onSubmit={formSubmitHandler}>
+				<input type="text" onChange={enteredNoteHandler} value={enteredNote} />
+				<button type="submit">Save</button>
+			</form>
 		</div>
 	)
 }
-
-
 
 export default App
