11c010756c17f49ba007d82a61c3afca3ed690f7
Lucas Gomes <lucasggomees@gmail.com>
2023-05-31 15:39:39 +0100

    Exercise 2.11 - Using json-server to simulate the fetch of data from a server

--- part2/notesExample/src/App.jsx
+++ part2/notesExample/src/App.jsx
@@ -1,70 +1,69 @@
-import React from 'react'
-import Note from './components/Note'
-import { useState } from 'react'
-
-const notes = [
-	{
-		id: 1,
-		content: 'HTML is easy',
-		important: true,
-	},
-	{
-		id: 2,
-		content: 'Browser can execute only JavaScript',
-		important: false,
-	},
-	{
-		id: 3,
-		content: 'GET and POST are the most important methods of HTTP protocol',
-		important: true,
-	},
-]
+import React from "react";
+import Note from "./components/Note";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
-	const [notesList, setNotesList] = useState(notes)
-	const [enteredNote, setEnteredNote] = useState('')
-	const [showAll, setShowAll] = useState(true)
+  const [notesList, setNotesList] = useState([]);
+  const [enteredNote, setEnteredNote] = useState("");
+  const [showAll, setShowAll] = useState(true);
 
-	const enteredNoteHandler = (e) => setEnteredNote(e.target.value)
-	const filteredNotes = showAll ? notesList : notesList.filter(note => note.important)
-	const filterHandler = ()=>setShowAll(!showAll)
+  const hook = () => {
+    console.log("useEffect fired");
+    axios.get("http://localhost:3001/notes").then((res) => {
+      //   console.log(res);
+      setNotesList(res.data);
+    });
+  };
 
-	// console.log(enteredNote)
+  useEffect(hook, []);
+  //   console.log("render", notesList.length, "notes");
 
-	const formSubmitHandler = (e) => {
-		e.preventDefault()
-		const newNoteData = {
-			id: Math.random(),
-			content: enteredNote,
-			important: Math.random() < 0.5,
-		}
-		setNotesList(notesList.concat(newNoteData)) // setNotesList([...notesList, newNoteData]))
-		// console.log(newNoteData)
-		// console.log(notesList)
-		setEnteredNote('')
-	}
+  const enteredNoteHandler = (e) => setEnteredNote(e.target.value);
+  const filteredNotes = showAll
+    ? notesList
+    : notesList.filter((note) => note.important);
+  const filterHandler = () => setShowAll(!showAll);
 
+  // console.log(enteredNote)
 
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    const newNoteData = {
+      id: Math.random(),
+      content: enteredNote,
+      important: Math.random() < 0.5,
+    };
+    setNotesList(notesList.concat(newNoteData)); // setNotesList([...notesList, newNoteData]))
+    // console.log(newNoteData)
+    // console.log(notesList)
+    setEnteredNote("");
+  };
 
-	return (
-		<div>
-			<h1>Notes</h1>
-			<div>
-				<button onClick={filterHandler}>
-					Showing {showAll?'all':'important'} notes
-				</button>
-			</div>
-			<ul>
-				{filteredNotes.map((note) => (
-					<Note key={note.id} note={note} />
-				))}
-			</ul>
-			<form onSubmit={formSubmitHandler}>
-				<input type="text" onChange={enteredNoteHandler} value={enteredNote} placeholder='Enter new note'/>
-				<button type="submit">Save</button>
-			</form>
-		</div>
-	)
-}
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={filterHandler}>
+          Showing {showAll ? "all" : "important"} notes
+        </button>
+      </div>
+      <ul>
+        {filteredNotes.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+      <form onSubmit={formSubmitHandler}>
+        <input
+          type="text"
+          onChange={enteredNoteHandler}
+          value={enteredNote}
+          placeholder="Enter new note"
+        />
+        <button type="submit">Save</button>
+      </form>
+    </div>
+  );
+};
 
-export default App
+export default App;
--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,78 +1,77 @@
-import React from 'react'
-import { useState } from 'react'
-import SearchFilter from './components/SearchFilter'
-import NewContactForm from './components/NewContactForm'
-import DisplayContacts from './components/DisplayContacts'
-
-const peopleList = [
-	{ id: '1', name: 'Arto Hellas', number: '040-123456' },
-	{ id: '2', name: 'Ada Lovelace', number: '39-44-5323523' },
-	{ id: '3', name: 'Dan Abramov', number: '12-43-234345' },
-	{ id: '4', name: 'Mary Poppendieck', number: '39-23-6423122' },
-	{ id: '5', name: 'John Doe', number: '07758899268' },
-]
+import React from "react";
+import { useState, useEffect } from "react";
+import SearchFilter from "./components/SearchFilter";
+import NewContactForm from "./components/NewContactForm";
+import DisplayContacts from "./components/DisplayContacts";
+import axios from "axios";
 
 function App() {
-	const [persons, setPersons] = useState(peopleList)
-	const [personsFilter, setPersonsFilter] = useState('')
-	const [newContactObj, setNewContactObj] = useState({
-		id: '',
-		name: '',
-		number: '',
-	})
+  const [persons, setPersons] = useState([]);
+  const [personsFilter, setPersonsFilter] = useState("");
+  const [newContactObj, setNewContactObj] = useState({
+    id: "",
+    name: "",
+    number: "",
+  });
 
-	const newContactNameHandler = (e) =>
-		setNewContactObj({ ...newContactObj, name: e.target.value })
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/contacts")
+      .then((res) => setPersons(res.data));
+  });
 
-	const newContactNumberHandler = (e) =>
-		setNewContactObj({ ...newContactObj, number: e.target.value })
+  const newContactNameHandler = (e) =>
+    setNewContactObj({ ...newContactObj, name: e.target.value });
 
-	const filterHandler = (e) => {
-		setPersonsFilter(e.target.value)
-	}
+  const newContactNumberHandler = (e) =>
+    setNewContactObj({ ...newContactObj, number: e.target.value });
 
-	const submitHandler = (e) => {
-		e.preventDefault()
-		const newContactData = {
-			id: Math.random().toString(),
-			name: newContactObj.name,
-			number: newContactObj.number,
-		}
-		const itExists = persons.find(
-			(value) => JSON.stringify(value) === JSON.stringify(newContactData)
-		)
-		!itExists && newContactObj.name !== '' && newContactObj.number !== ''
-			? setPersons(persons.concat(newContactData))
-			: alert(
-					`The contact ${newContactObj.name} already exists in the phonebook or input is invalid`
-			  )
-		// console.log(itExists, newContactData, persons)
-		setNewContactObj({
-			id: '',
-			name: '',
-			number: '',
-		})
-	}
+  const filterHandler = (e) => {
+    setPersonsFilter(e.target.value);
+  };
 
-	const filteredPersonList = persons.filter((person) =>
-		person.name.toUpperCase().includes(personsFilter.toUpperCase())
-	)
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const newContactData = {
+      id: Math.random().toString(),
+      name: newContactObj.name,
+      number: newContactObj.number,
+    };
+    const itExists = persons.find(
+      (value) => JSON.stringify(value) === JSON.stringify(newContactData)
+    );
+    !itExists && newContactObj.name !== "" && newContactObj.number !== ""
+      ? setPersons(persons.concat(newContactData))
+      : alert(
+          `The contact ${newContactObj.name} already exists in the phonebook or input is invalid`
+        );
+    // console.log(itExists, newContactData, persons)
+    setNewContactObj({
+      id: "",
+      name: "",
+      number: "",
+    });
+  };
 
-	return (
-		<div>
-			<h2>Phonebook</h2>
-			<SearchFilter filterHandler={filterHandler} />
-			<h2>Add new contact</h2>
-			<NewContactForm
-				submitHandler={submitHandler}
-				newContactNameHandler={newContactNameHandler}
-				newContactObj={newContactObj}
-				newContactNumberHandler={newContactNumberHandler}
-			/>
-			<h2>Contacts</h2>
-			<DisplayContacts filteredPersonList={filteredPersonList} />
-		</div>
-	)
+  const filteredPersonList = persons.filter((person) =>
+    person.name.toUpperCase().includes(personsFilter.toUpperCase())
+  );
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <SearchFilter filterHandler={filterHandler} />
+      <h2>Add new contact</h2>
+      <NewContactForm
+        submitHandler={submitHandler}
+        newContactNameHandler={newContactNameHandler}
+        newContactObj={newContactObj}
+        newContactNumberHandler={newContactNumberHandler}
+      />
+      <h2>Contacts</h2>
+      <DisplayContacts filteredPersonList={filteredPersonList} />
+    </div>
+  );
 }
 
-export default App
+export default App;
